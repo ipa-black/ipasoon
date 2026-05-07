2a0ae1dc0bbd4ae0a0d31df4b097744d7d4c8911
ipa-black <ccf17ih@gmail.com>
2026-05-07 21:11:03 +0300

    Update install.js

--- api/install.js
+++ api/install.js
@@ -2,72 +2,22 @@ export default function handler(req, res) {
     const { name, ipa_url, bundle_id, image_url } = req.query;
 
     if (!name || !ipa_url || !bundle_id) {
-        return res.status(400).send('عذراً، معلومات التطبيق غير مكتملة.');
+        return res.status(400).send('Missing parameters');
     }
 
-    // تنظيف الرموز لتجنب أخطاء نظام iOS
-    const escapeXml = (unsafeStr) => {
-        if (!unsafeStr) return '';
-        return unsafeStr.replace(/[<>&'"]/g, (char) => {
-            switch (char) {
-                case '<': return '&lt;';
-                case '>': return '&gt;';
-                case '&': return '&amp;';
-                case '\'': return '&apos;';
-                case '"': return '&quot;';
-                default: return char;
-            }
-        });
-    };
+    const escapeXml = (unsafe) => unsafe.replace(/[<>&'"]/g, c => 
+        ({'<': '&lt;', '>': '&gt;', '&': '&amp;', "'": '&apos;', '"': '&quot;'}[c]));
 
     const safeName = escapeXml(name);
-    const safeIpaUrl = escapeXml(ipa_url);
-    const safeBundleId = escapeXml(bundle_id);
-    const safeImageUrl = escapeXml(image_url || 'https://via.placeholder.com/100');
+    const safeIpa = escapeXml(ipa_url);
+    const safeBid = escapeXml(bundle_id);
+    const safeImg = escapeXml(image_url || '');
 
     const plist = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-    <key>items</key>
-    <array>
-        <dict>
-            <key>assets</key>
-            <array>
-                <dict>
-                    <key>kind</key>
-                    <string>software-package</string>
-                    <key>url</key>
-                    <string>${safeIpaUrl}</string>
-                </dict>
-                <dict>
-                    <key>kind</key>
-                    <string>display-image</string>
-                    <key>needs-shine</key>
-                    <true/>
-                    <key>url</key>
-                    <string>${safeImageUrl}</string>
-                </dict>
-            </array>
-            <key>metadata</key>
-            <dict>
-                <key>bundle-identifier</key>
-                <string>${safeBundleId}</string>
-                <key>bundle-version</key>
-                <string>1.0</string>
-                <key>kind</key>
-                <string>software</string>
-                <key>title</key>
-                <string>${safeName}</string>
-            </dict>
-        </dict>
-    </array>
-</dict>
-</plist>`;
+<plist version="1.0"><dict><key>items</key><array><dict><key>assets</key><array><dict><key>kind</key><string>software-package</string><key>url</key><string>${safeIpa}</string></dict><dict><key>kind</key><string>display-image</string><key>url</key><string>${safeImg}</string></dict></array><key>metadata</key><dict><key>bundle-identifier</key><string>${safeBid}</string><key>bundle-version</key><string>1.0</string><key>kind</key><string>software</string><key>title</key><string>${safeName}</string></dict></dict></array></dict></plist>`;
 
-    res.setHeader('Content-Type', 'text/xml; charset=utf-8');
-    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.setHeader('Content-Disposition', 'inline; filename="install.plist"');
-    
+    res.setHeader('Content-Type', 'text/xml');
+    res.setHeader('Cache-Control', 'no-store');
     res.status(200).send(plist);
 }
